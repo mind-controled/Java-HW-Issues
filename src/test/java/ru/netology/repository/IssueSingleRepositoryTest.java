5314b733e6e82607027f1520b5729e5dc40eab24
mind_control <mind_control@mail.ru>
2020-05-15 15:16:30 +0300

    Date type changed

--- src/test/java/ru/netology/repository/IssueSingleRepositoryTest.java
+++ src/test/java/ru/netology/repository/IssueSingleRepositoryTest.java
@@ -8,6 +8,7 @@ import ru.netology.domain.Label;
 import ru.netology.domain.Status;
 import ru.netology.exception.NotFoundException;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -17,15 +18,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class IssueSingleRepositoryTest {
-
     private IssueRepository issueRepository = new IssueRepository();
-
-    private Issue issue1 = new Issue(1, "name1", Status.OPEN, "author1", Label.BUG, Arrays.asList("project1", "project2"), "5.7 M2", new Assignee(4, "Name4", "Surname4"), "26.01.2020", 3, 4);
+    private Issue issue1 = new Issue(1, "name1", Status.OPEN, "author1", Label.BUG, Arrays.asList("project1", "project2"), "5.7 M2", new Assignee(4, "Name4", "Surname4"), LocalDate.of(2020, 3, 1), 3, 4);
 
     @BeforeEach()
     void setup() {
         issueRepository.save(issue1);
     }
+
     @Test
     void shouldFindAll() {
         ArrayList<Issue> actual = issueRepository.findAll();
